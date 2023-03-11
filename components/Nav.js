75ea9b0afff5548cac8d10e7805dd15dc406ab65
Jags <jagsmk2.0@gmail.com>
2023-03-12 01:36:47 +0530

    update

--- components/Nav.js
+++ components/Nav.js
@@ -7,35 +7,179 @@ const Nav = () => {
     return (
         <div className={styles.myNav}>
             <Link href="/">
-            <img src="https://w7.pngwing.com/pngs/621/196/png-transparent-e-commerce-logo-logo-e-commerce-electronic-business-ecommerce-angle-text-service.png" 
+            <img className={styles.myNav_logo} src="https://w7.pngwing.com/pngs/621/196/png-transparent-e-commerce-logo-logo-e-commerce-electronic-business-ecommerce-angle-text-service.png" 
             alt='logo' height='50px' width="50px"/>
             </Link>
+            <div className={styles.myNav_home}>
             <Link href="/" legacyBehavior>
                 <a>Home</a>
             </Link>
+            </div>
             
+            <div className={styles.dropdown}>
             <Link href="/product" legacyBehavior>
                 <a>Product</a>
             </Link>
+            <div className={styles.menu}>
+                
+                <div className={styles.firstChild}>
+                <p className={styles.para}>Boys</p>
+                    <div>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    </div>
+                </div>
+                
+                <div className={styles.firstChild}>
+                <p className={styles.para}>Boys</p>
+                <div>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    </div>
+                </div>
+                
+                <div className={styles.firstChild}>
+                <p className={styles.para}>Boys</p>
+                <div>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    </div>
+                </div>
+                
+                <div className={styles.firstChild}>
+                <p className={styles.para}>Boys</p>
+                <div>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    <p className={styles.dropmenu}>Shirt</p>
+                    </div>
+                </div>
+            </div>
+            </div>
 
+            <div className={styles.dropdown}>
             <Link href='/product' legacyBehavior>
                 <a>Decor</a>
             </Link>
+            <div className={styles.menu}>
+                
+                <div className={styles.firstChild}>
+                <p className={styles.para}>Boys</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                </div>
+                
+                <div className={styles.firstChild}>
+                <p className={styles.para}>Boys</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                </div>
+                
+                <div className={styles.firstChild}>
+                <p className={styles.para}>Boys</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                </div>
+                
+                <div className={styles.firstChild}>
+                <p className={styles.para}>Boys</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                </div>
+            </div>
+            </div>
 
+            <div className={styles.dropdown}>
             <Link href='/product' legacyBehavior>
                 <a>Items</a>
             </Link>
+            <div className={styles.menu}>
+                
+                <div className={styles.firstChild}>
+                <p className={styles.para}>Boys</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                </div>
+                
+                <div className={styles.firstChild}>
+                <p className={styles.para}>Boys</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                </div>
+                
+                <div className={styles.firstChild}>
+                <p className={styles.para}>Boys</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                </div>
+                
+                <div className={styles.firstChild}>
+                <p className={styles.para}>Boys</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                    <p>Shirt</p>
+                </div>
+            </div>
+            </div>
 
-            <input type={`text`} placeholder='search the store'
+            <div className={styles.nav}>
+            <input className={styles.myNav_search} type={`text`} placeholder='search the store'
             onButtonClick={buttonClick}/>
 
-            <Link href='/profile' legacyBehavior>
+            <Link className={styles.myNav_profile} href='/profile' legacyBehavior>
                 <a>Profile</a>
             </Link>
 
-            <Link href='/wishlist' legacyBehavior>
+            <Link className={styles.myNav_cart} href='/wishlist' legacyBehavior>
                 <a>Cart</a>
             </Link>
+            </div>
 
 {/* Scoped css */}
             <style jsx>{`
@@ -49,9 +193,7 @@ const Nav = () => {
             a:hover{
                 border-bottom: 2px solid orange;
             }
-            input{
-                border: 2px solid red;
-            }
+           
             img{
                 margin-top: 5px;
             }
